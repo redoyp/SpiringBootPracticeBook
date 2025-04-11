b31e1323925062afc5aa8ff48b7cf13f962a6b21
redoyp <gsolu0302@gmail.com>
2025-04-11 15:29:37 +0900

    RestTemplate 외부 api 불러오기

--- src/main/java/com/example/books/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/books/configuration/WebSecurityConfig.java
@@ -10,6 +10,6 @@ import org.springframework.context.annotation.Bean;
 public class WebSecurityConfig {
     @Bean
     public WebSecurityCustomizer configure() {      // 스프링 시큐리티 기능 비활성화
-        return web -> web.ignoring().requestMatchers("/static/**", "/books/**"); // 개발하면서 잠깐 끄고 싶으면 and 뒤에 이런 식으로 붙이면 됨
+        return web -> web.ignoring().requestMatchers("/static/**", "/books/**", "/api/external"); // 개발하면서 잠깐 끄고 싶으면 and 뒤에 이런 식으로 붙이면 됨
     }
 }
